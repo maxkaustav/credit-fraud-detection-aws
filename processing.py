624a59c5af235aa9ffb9bc2225526b2e28bcffcb
maxkaustav <kaustav28ay@gmail.com>
2025-11-20 15:02:02 +0000

    First push

---
+++ processing.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+import boto3
+
+s3 = boto3.resource('s3', region_name=boto3.Session().region_name)
+bucket_name = 'sandboxstav'
+
+data = pd.read_csv('/opt/ml/processing/input/creditcard.csv', delimiter=',')
+
+feature_columns = data.columns[:-1]
+label_column = data.columns[-1]
+
+features = data[feature_columns].values.astype('float32')
+labels = (data[label_column].values).astype('float32')
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(
+    features, labels, test_size=0.1, random_state=42)
+
+
+np.save("/opt/ml/processing/output/X_test.npy", X_test)
+np.save("/opt/ml/processing/output/y_test.npy", y_test)
+
+
+
+print("Saving Transformed train data to: '/opt/ml/processing/output/train' ...")
+
+np.save("/opt/ml/processing/output/X_train.npy", X_train)
+np.save("/opt/ml/processing/output/y_train.npy", y_train)
+
+
+print("Saving Transformed test data to: '/opt/ml/processing/output/test' ...")
+
+np.save("/opt/ml/processing/output/X_test.npy", X_test)
+np.save("/opt/ml/processing/output/y_test.npy", y_test)
+
+print("completed processing ............")
